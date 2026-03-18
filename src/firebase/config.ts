3f85221f85f877605d0ad7e68987a9128e17bc1d
Emil Wawrzycki <emil.wawrzycki@ewakon.pl>
2026-03-18 18:28:34 +0000

    Fix: complete Firebase API key and appId

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,13 +3,13 @@ import { getDatabase } from 'firebase/database';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbfnkQiGvjpRJrgt_z6P-1h4GQ",
+  apiKey: "AIzaSyBbfnkQiGvjpRJrgt_z6P-1h4GQAY11sOo",
   authDomain: "qwirkle-online-6ca1c.firebaseapp.com",
   databaseURL: "https://qwirkle-online-6ca1c-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "qwirkle-online-6ca1c",
   storageBucket: "qwirkle-online-6ca1c.firebasestorage.app",
   messagingSenderId: "712381168296",
-  appId: "1:712381168296:web:2421aa1b3619d0"
+  appId: "1:712381168296:web:2421aa1b3619d0c0a4609d"
 };
 
 const app = initializeApp(firebaseConfig);
